d702bd701a8f28ab54ba391e631480277a07da1b
JOHAN DE GROOT <johde234@student.liu.se>
2018-10-08 11:15:58 +0200

    Updated the News page with more news from week 40.

--- c3web/src/components/News.js
+++ c3web/src/components/News.js
@@ -47,11 +47,30 @@ renderText = () => {
           websitemadeMob
           return(
             <p style={STYLE}> 
-            <span style={p1STYLE}>Week 37: </span>
+            <span style={p1STYLE}>Week 40: </span>
+            Welcome to the last week of sprint 1. The development team has made great progress on the
+            product during last week and it will be fun to see the result during the delivery this week.
+            All the teams have really brought their a-game to bring together our product. Keep up the good work and high spirit! <br/> <br/>
+            //Project Manager Ellika Jernå <br/> <br/>
+            <span style={p1STYLE}>Week 39: </span>
+            The highlight for week 39 was the succesfull tollgate meeting with Zenterio. The management team did a great job presenting our product idea! <br/> <br/>
+            It has been a very productive week and we have made a decision regarding our product name with designs, created a first-draft prototype, 
+            finished our backlog with time estimation and set a delivery plan of four sprints. Our plans for next week is to begin the development of sprint 1! <br/> <br/>
+            //Project Manager Ellika Jernå <br/> <br/>
+           
+            <span style={p1STYLE}>Week 38: </span>
+            During this week we held a kick-off containing music quiz, pizza eating and socialization. Very inspiring!
+            We formed a group contract, decided on technical environment, created a System Requirements Specification, Inspection Plan and a prototype.
+            To make the development work more structured we decided to have a lead developer front-end and a lead developer back-end.
+            Johan de Groot held a workshop in React.js. <br/>
+            The big happening for next week is the tollgate meeting with Zenterio! <br/> <br/>
+            //Project Manager Ellika Jernå <br/> <br/>
+            
+             <span style={p1STYLE}>Week 37: </span>
             The project is now in full motion and we have together with Zenterio decided to continue our work by developing a product that will 
             boost its catalogue by promoting games based on its popularity in a social video platform. 
-            This will be done by incorporating Twitch.TV and Steam to create an HTML5 application. 
-
+            This will be done by incorporating Twitch.TV and Steam to create an HTML5 application. <br/> <br/>
+            //Project Manager Ellika Jernå 
         
             </p>
 
